8d8b478dbf900f217657894386c6dc4a1df44e8e
linph <370345882@qq.com>
2018-07-10 15:50:54 +0800

    add static template

---
+++ src/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = `
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Koa Server Html</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+    <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body>
+    <div className="container">
+      <div class="row">
+        <div class="col-md-8">电影预告</div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">linph</div>
+      </div>
+    
+    </div>
+  </body>
+</html>
+`
