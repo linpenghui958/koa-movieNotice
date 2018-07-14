93e8e67ad42364b465f03de2c8e41e2f0fbc47a1
linph <370345882@qq.com>
2018-07-14 23:41:15 +0800

    mongoose test

---
+++ src/database/init.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const dbUrl = 'mongodb://localhost/douban-movie'
+mongoose.Promise = global.Promise 
+
+exports.connect = () => {
+  return new Promise((resolve, reject) => {
+
+    let maxConnectTimes = 0
+
+    if (process.env.NODE_ENV !== 'production') {
+      mongoose.set('debug', true)
+    }
+
+    mongoose.connect(dbUrl)
+
+    const db = mongoose.connection
+
+    db.on('error', (err) => {
+      maxConnectTimes++
+      if (maxConnectTimes < 5) {
+        mongoose.connect(dbUrl)
+      } else {
+        throw new Error('你的数据库好像挂了啊！')
+      }
+    })
+
+    db.once('open', () => {
+      const Dog = mongoose.model('Dog', {name: String})
+      const doga = new Dog({name: 'chungua'})
+
+      doga.save().then(() => {
+        console.log('save success')
+      })
+      resolve()
+      console.log('数据库连接成功！')
+    })
+
+    db.on('disconnected', () => {
+      maxConnectTimes++
+      if (maxConnectTimes < 5) {
+        mongoose.connect(dbUrl)
+      } else {
+        throw new Error('你的数据库好像挂了啊！')
+      }
+    })
+
+  })
+}
--- test/readFile.js
+++ test/readFile.js
@@ -1,12 +1,106 @@
-const { readFile } = require('fs')
+// const { readFile } = require('fs')
 
-let time1 = new Date().getTime()
+// let time1 = new Date().getTime()
 
-readFile('../img.png', 'utf-8', data => {
-  console.log('完成文件 1 读操作的回调')
-  let time2 = new Date().getTime()
-  let totalTime = time2 - time1
-  console.log(time1)
-  console.log(time2)
-  console.log(`总共耗时${totalTime}`)
+// readFile('../img.png', 'utf-8', data => {
+//   console.log('完成文件 1 读操作的回调')
+//   let time2 = new Date().getTime()
+//   let totalTime = time2 - time1
+//   console.log(time1)
+//   console.log(time2)
+//   console.log(`总共耗时${totalTime}`)
+// })
+
+// console.log(1)
+
+// setTimeout(() => {
+//     console.log(2)
+//     new Promise(resolve => {
+//         console.log(4)
+//         resolve()
+//     }).then(() => {
+//         console.log(5)
+//     })
+// })
+
+// new Promise(resolve => {
+//     console.log(7)
+//     resolve()
+// }).then(() => {
+//     console.log(8)
+// })
+
+// setTimeout(() => {
+//     console.log(9)
+//     new Promise(resolve => {
+//         console.log(11)
+//         resolve()
+//     }).then(() => {
+//         console.log(12)
+//     })
+// })
+
+
+// console.log(1)
+
+// setTimeout(() => {
+//     console.log(2)
+//     new Promise(resolve => {
+//         console.log(4)
+//         resolve()
+//     }).then(() => {
+//         console.log(5)
+//     })
+//     process.nextTick(() => {
+//         console.log(3)
+//     })
+// })
+
+// new Promise(resolve => {
+//     console.log(7)
+//     resolve()
+// }).then(() => {
+//     console.log(8)
+// })
+
+// process.nextTick(() => {
+//     console.log(6)
+// })
+
+console.log('golb');
+
+setTimeout(function() {
+    console.log('timeout');
+    process.nextTick(function() {
+        console.log('timeout_nextTick');
+    })
+    new Promise(function(resolve) {
+        console.log('timeout_promise');
+        resolve();
+    }).then(function() {
+        console.log('timeout_then')
+    })
+})
+
+setImmediate(function() {
+    console.log('immediate');
+    process.nextTick(function() {
+        console.log('immediate_nextTick');
+    })
+    new Promise(function(resolve) {
+        console.log('immediate_promise');
+        resolve();
+    }).then(function() {
+        console.log('immediate_then')
+    })
+})
+
+process.nextTick(function() {
+    console.log('glob_nextTick');
+})
+new Promise(function(resolve) {
+    console.log('glob_promise');
+    resolve();
+}).then(function() {
+    console.log('glob_then')
 })
