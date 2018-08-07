b8004c191079805ba3faec00f8efd775b4b9456e
linph <370345882@qq.com>
2018-08-07 22:45:15 +0800

    modify crawler

--- server/crawler/fetchAndUpload.js
+++ server/crawler/fetchAndUpload.js
@@ -80,6 +80,8 @@ async function fetchMovieData (item) {
   })
 
   const page = await browser.newPage()
+
+  //爬取预告列表
   // await page.goto(movieListUrl, {
   //   waitUntil: 'networkidle2'
   // })
@@ -130,170 +132,166 @@ async function fetchMovieData (item) {
   // })
 
   //从数据库中获取movieList
-  let movieDataList = await Movie.find({
+  // let movieDataList = await Movie.find({
+  //   $or: [
+  //     { video: { $exists: false} }
+  //   ]
+  // }).sort({meta: -1}).limit(100)
+
+  // console.log(`需要爬取video的数据长度为${movieDataList.length}`)
+
+  // let fetchMovieDataList = []
+
+  // for ( let i = 0; i < movieDataList.length; i++) {
+  //   let movie = movieDataList[i]
+  //   const doubanId = movie.doubanId
+  //   console.log(`Start trailer NO.${i} doubanId${doubanId}-${movie.title} video source`)
+
+  //   await page.goto(videoBaseUrl + doubanId, {
+  //     waitUntil: 'networkidle2'
+  //   })
+
+  //   await sleep(1000)
+
+  //   const result = await page.evaluate(() => {
+  //     const $ = window.$
+  //     const $video = $('.related-pic-video')
+  //     if ($video && $video.length > 0) {
+  //       var link = $video.attr('href')
+  //       var coverImg = $video.css('background-image').match(/(?<=").*(?=")/)[0]
+  //     }
+  //     return {link, coverImg}
+  //   })
+  //   let video
+  //   if (result.link) {
+  //     await page.goto(result.link, {
+  //       waitUntil: 'networkidle2'
+  //     })
+  //     video = await page.evaluate(() => {
+  //       const $ = window.$
+  //       const $source = $('source')
+  //       if ($source && $source.length > 0) {
+  //         return $source.attr('src')
+  //       }
+  //     })
+  //   }
+  //   if (video) {
+  //     movie.video = video
+  //     movie.link = result.link
+  //     movie.image = result.coverImg
+  //     console.log(`保存${movie.title}数据`)
+  //     await movie.save()
+  //   }
+
+  //   // 上传对应数据至qiniu
+  //   console.log(`fetch upload NO. ${i} movie resource`)
+  //   try {
+  //     console.log('start upload doubanId' + movie.doubanId + 'resource')
+  //     let videoData = await fetchAndUploadToQiniu(movie.video, nanoid() + '.mp4')
+  //     let coverData = await fetchAndUploadToQiniu(movie.image, nanoid() + '.png')
+  //     let posterData = await fetchAndUploadToQiniu(movie.poster, nanoid() + '.png')
+    
+  //     if (videoData.key)
+  //     videoData.key && (movie.videoKey = videoData.key)
+  //     coverData.key && (movie.coverKey = coverData.key)
+  //     posterData.key && (movie.posterKey = posterData.key)
+
+  //     console.log(`上传七牛并保存${movie.doubanid}-${movie.title}   resource`)
+  //     await movie.save()
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // }
+
+
+
+  //从豆瓣api完善电影数据
+    
+  // 获取需要完善的电影列表
+  let movies = await Movie.find({
     $or: [
-      { video: { $exists: false} }
+      { summary: { $exists: false } },
+      { summary: null },
+      { year: { $exists: false } },
+      { title: '' },
+      { summary: '' }
     ]
-  }).sort({meta: -1}).limit(100)
+  })
 
-  console.log(`需要爬取video的数据长度为${movieDataList.length}`)
+  for (let i = 0; i < 70; i++) {
+  // for (let i = 0; i < 1; i++) {
+    await sleep(5000)
+    let movie = movies[i]
+    let movieData = await fetchMovieData(movie)
 
-  let fetchMovieDataList = []
+    if (movieData) {
+      let tags = movieData.tags || []
 
-  for ( let i = 0; i < movieDataList.length; i++) {
-    let movie = movieDataList[i]
-    const doubanId = movie.doubanId
-    console.log(`Start trailer NO.${i} doubanId${doubanId}-${movie.title} video source`)
+      movie.tags = movie.tags || []
+      movie.summary = movieData.summary || ''
+      movie.title = movieData.alt_title || movieData.title || ''
+      movie.rawTitle = movieData.title || ''
 
-    await page.goto(videoBaseUrl + doubanId, {
-      waitUntil: 'networkidle2'
-    })
+      if (movieData.attrs) {
+        movie.movieTypes = movieData.attrs.movie_type || []
+        movie.year = movieData.attrs.year[0] || 2500
 
-    await sleep(1000)
+        for (let i = 0; i < movie.movieTypes.length; i++) {
+          let item = movie.movieTypes[i]
+          let cat = await Category.findOne({
+            name: item
+          })
 
-    const result = await page.evaluate(() => {
-      const $ = window.$
-      const $video = $('.related-pic-video')
-      if ($video && $video.length > 0) {
-        var link = $video.attr('href')
-        var coverImg = $video.css('background-image').match(/(?<=").*(?=")/)[0]
-      }
-      return {link, coverImg}
-    })
-    let video
-    if (result.link) {
-      await page.goto(result.link, {
-        waitUntil: 'networkidle2'
-      })
-      video = await page.evaluate(() => {
-        const $ = window.$
-        const $source = $('source')
-        if ($source && $source.length > 0) {
-          return $source.attr('src')
+          if (!cat) {
+            cat = new Category({
+              name: item,
+              movies: [movie._id]
+            })
+          } else {
+            if (cat.movies.indexOf(movie._id) === -1) {
+              cat.movies.push(movie._id)
+            }
+          }
+
+          await cat.save()
+
+          if (!movie.category) {
+            movie.category.push(cat._id)
+          } else {
+            if (movie.category.indexOf(cat._id) === -1) {
+              movie.category.push(cat._id)
+            }
+          }
         }
-      })
+
+        let dates = movieData.attrs.pubdate || []
+        let pubdates = []
+        dates.map(item => {
+          if (item && item.split('(').length > 0) {
+            let parts = item.split('(')
+            let date = parts[0]
+            let country = '未知'
+
+            if (parts[1]) {
+              country = parts[1].split(')')[0]            
+            }
+
+            pubdates.push({
+              date: new Date(date),
+              country
+            })
+
+          }
+          movie.pubdate = pubdates
+        })
+
+        tags.forEach(tag => {
+          movie.tags.push(tag.name)
+        })
+
+        await movie.save()
+      }
     }
-    if (video) {
-      movie.video = video
-      movie.link = result.link
-      movie.image = result.coverImg
-      console.log(`保存${movie.title}数据`)
-      await movie.save()
-    }
-
-    // 上传对应数据至qiniu
-    console.log(`fetch upload NO. ${i} movie resource`)
-    try {
-      console.log('start upload doubanId' + movie.doubanId + 'resource')
-      let videoData = await fetchAndUploadToQiniu(movie.video, nanoid() + '.mp4')
-      let coverData = await fetchAndUploadToQiniu(movie.image, nanoid() + '.png')
-      let posterData = await fetchAndUploadToQiniu(movie.poster, nanoid() + '.png')
-    
-      if (videoData.key)
-      videoData.key && (movie.videoKey = videoData.key)
-      coverData.key && (movie.coverKey = coverData.key)
-      posterData.key && (movie.posterKey = posterData.key)
-
-      console.log(`上传七牛并保存${movie.doubanid}-${movie.title}   resource`)
-      await movie.save()
-    } catch (e) {
-      console.log(e)
-    }
-
-    //从豆瓣api完善电影数据
-    
-    // 获取需要完善的电影列表
-    // let movies = await Movie.find({
-    //   $or: [
-    //     { summary: { $exists: false } },
-    //     { summary: null },
-    //     { year: { $exists: false } },
-    //     { title: '' },
-    //     { summary: '' }
-    //   ]
-    // })
-
-    // for (let i = 0; i < 70; i++) {
-    //   // for (let i = 0; i < 1; i++) {
-    //     await sleep(5000)
-    //     let movie = movies[i]
-    //     let movieData = await fetchMovieData(movie)
-    
-    //     if (movieData) {
-    //       let tags = movieData.tags || []
-    
-    //       movie.tags = movie.tags || []
-    //       movie.summary = movieData.summary || ''
-    //       movie.title = movieData.alt_title || movieData.title || ''
-    //       movie.rawTitle = movieData.title || ''
-    
-    //       if (movieData.attrs) {
-    //         movie.movieTypes = movieData.attrs.movie_type || []
-    //         movie.year = movieData.attrs.year[0] || 2500
-    
-    //         for (let i = 0; i < movie.movieTypes.length; i++) {
-    //           let item = movie.movieTypes[i]
-    //           let cat = await Category.findOne({
-    //             name: item
-    //           })
-    
-    //           if (!cat) {
-    //             cat = new Category({
-    //               name: item,
-    //               movies: [movie._id]
-    //             })
-    //           } else {
-    //             if (cat.movies.indexOf(movie._id) === -1) {
-    //               cat.movies.push(movie._id)
-    //             }
-    //           }
-    
-    //           await cat.save()
-    
-    //           if (!movie.category) {
-    //             movie.category.push(cat._id)
-    //           } else {
-    //             if (movie.category.indexOf(cat._id) === -1) {
-    //               movie.category.push(cat._id)
-    //             }
-    //           }
-    //         }
-    
-    //         let dates = movieData.attrs.pubdate || []
-    //         let pubdates = []
-    //         dates.map(item => {
-    //           if (item && item.split('(').length > 0) {
-    //             let parts = item.split('(')
-    //             let date = parts[0]
-    //             let country = '未知'
-    
-    //             if (parts[1]) {
-    //               country = parts[1].split(')')[0]            
-    //             }
-    
-    //             pubdates.push({
-    //               date: new Date(date),
-    //               country
-    //             })
-    
-    //           }
-    //           movie.pubdate = pubdates
-    //         })
-    
-    //         tags.forEach(tag => {
-    //           movie.tags.push(tag.name)
-    //         })
-    
-    //         await movie.save()
-    //       }
-    //     }
-    //   }
-
-
-
-
-
-
   }
 
   await browser.close()
